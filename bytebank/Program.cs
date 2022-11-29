8370b701b6844f0d54cae05c56f80bdda48b8800
Thiago Tomé <dev.tomethiago@gmail.com>
2022-11-29 18:06:29 -0300

    first commit

---
+++ bytebank/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank;
+
+ContaCorrente conta = new()
+{
+    titular = "Thiago Tomé",
+    numero_agencia = 1,
+    conta = "1223595",
+    saldo = 100
+};
+
+
+Console.WriteLine(conta.titular);
+Console.WriteLine(conta.numero_agencia);
+Console.WriteLine(conta.conta);
+Console.WriteLine(conta.saldo);
+
+Console.WriteLine("Antes do saque: R$ " + conta.saldo);
+conta.Sacar(50);
+Console.WriteLine("Depois do saque: R$ " + conta.saldo);
